b024301454cec30247441b6c1365a97de0f350b3
Godoy <josemariagodoy1996@gmail.com>
2024-12-29 21:10:08 +0100

    Proyecto 34, Correccion del 33

---
+++ Proyectos/HTML-CSS-JavaScript/Proyecto-34/main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+
+    let main = document.querySelector(".layout__main");
+    let messages = document.querySelector(".main__messages");
+    let form = document.querySelector(".main__form");
+    let input = document.querySelector(".form__input");
+    let loader = document.querySelector(".layout__loader");
+
+    let sendMessage = (e) => {
+        e.preventDefault();
+
+        let text = input.value;
+
+        if (text.trim() != "") {
+            messages.innerHTML += `
+                <article class="messages__message">
+                    <p class="message__name">Jose Maria Godoy</p>
+                    <p class="message__text">${text}</p>
+                </article>
+            `;
+
+            input.value = "";
+        }
+    };
+
+    setTimeout(() => {
+        loader.style.display = "none";
+        main.style.display = "flex";
+    }, 5000)
+
+    form.addEventListener("submit", sendMessage);
+});
